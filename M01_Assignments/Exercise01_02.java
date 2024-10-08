a4c17107db0ea55663c07e335e4ab9739be777c1
Kate Robinson <hodgekat@iu.edu>
2024-10-08 15:09:28 -0500

    Uploaded M01 Programs into M01_Assignments Folder

---
+++ M01_Assignments/Exercise01_02.java
@@ -0,0 +1,8 @@
+public class Exercise01_02 {
+    public static void main(String[] args) {
+        // Print "Welcome to Java" five times
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java");
+        }
+    }
+}
